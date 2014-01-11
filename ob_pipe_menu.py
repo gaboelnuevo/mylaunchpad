0a4cd4b8bcb262c4ec49a76906a26999832686d6
Gabriel S. L <gabo.elnuevo@googlemail.com>
2014-01-11 10:42:02 -0600

    Create ob_pipe_menu.py
    You can recycle this code to create a dinamic openbox pipe menu

---
+++ ob_pipe_menu.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#Recycle code for dynamic openbox pipe menu:
+
+import os
+import threading
+from appsmenu import MenuCache
+
+try:
+    from lxml import etree
+except:
+    print "lxml missing, please install pyhton-lxml "
+
+
+class OBMenu:
+    def __init__(self):
+        
+        # Get Menu
+	menu = MenuCache(AUTO_UPDATE=False, tag="openbox_pipe_menu", cache_dir = "openbox",  file_name = "openbox-xdg-menu-cache.xml")
+        self.appsmenu = menu.getMenu()
+
+        #update cache in the background
+        t = threading.Thread(target=menu.updateCache())  
+        t.start() 
+
+    def getMenu(self):
+       root = etree.parse(self.appsmenu)
+       print etree.tostring(root, pretty_print=True, encoding='UTF-8')
+
+menu = OBMenu()
+menu.getMenu()
