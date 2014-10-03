5d480e52e279c740eab78e9bceb8917073066dfd
Gabriel S. L <gaboelnuevo@users.noreply.github.com>
2014-10-03 14:16:49 -0600

    updated version to 0.3

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ except:
     
           
 setup(name="Launcher",  
-          version="0.2",  
+          version="0.3",  
           description="Eyecandy Full Screen launcher script for openbox and other stand-alone window managers.",  
           author="Gabriel Sabillon",  
           author_email="gabrielsabillon@gmail.com",  
